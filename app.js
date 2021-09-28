aa53e2802025caefe94a4c5c0cd1b9aee366c849
geo-evm <molodanjob@gmail.com>
2021-09-28 11:43:08 +0300

    port

--- app.js
+++ app.js
@@ -47,4 +47,4 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "index.html"));
 });
 
-app.listen(3000, () => console.log("Server has been started on port 3000..."));
+// app.listen(3000, () => console.log("Server has been started on port 3000..."));
